f59651702e698b23db77a64646d635bea7ea49b0
«juliashavrina» <tifanys@yandex.ru>
2022-06-28 18:21:05 +0300

    Homework1

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿/*задача 2
+int a,b;
+Console.WriteLine("Input a first number:");
+a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Input a second number:");
+b = Convert.ToInt32 (Console.ReadLine());
+
+if (a>b)
+    Console.WriteLine(a + "max");
+else 
+ {
+    Console.WriteLine( b + "max");
+ }
+ */
+
+/*задача 4
+int a, b, c;
+Console.WriteLine("Input a first number:");
+a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Input a second number:");
+b = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Input a third number:");
+c = Convert.ToInt32 (Console.ReadLine());
+
+int max = a;
+
+if (b > max) max=b;
+if ( c > max) max=c;
+Console.Write ("max=");
+Console.WriteLine (max);
+*/
+
+/* задача 6
+int a;
+int div;
+Console.WriteLine("Input a number:");
+a = Convert.ToInt32 (Console.ReadLine());
+
+div =  a / 2;
+
+if (div * 2 == a)
+    Console.WriteLine("yes");
+else
+    {
+   Console.WriteLine("no");
+    }   
+*/
+/* задача 8
+
+int N;
+Console.WriteLine("Input a number:");
+N = Convert.ToInt32 (Console.ReadLine());
+
+int current = 2;
+while (current < N)
+{
+    Console.Write(current +  " ");
+    current = current + 2;
+
+}
+*/
+
+
+    
+
+
+    
+    
+    
+   
+ 
+
+
